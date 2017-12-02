0c4d4b7d1588b99c4ad1bdb2addbb7e97b66cbb8
Emma Gordon <emmagordon@users.noreply.github.com>
2017-12-02 17:10:57 +0000

    Day 1 - Part 2

--- day1/captcha.py
+++ day1/captcha.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3.6
 
-"""The captcha requires you to review a sequence of digits and find the
-sum of all digits that match the next digit in the list. The list is
-circular, so the digit after the last digit is the first digit in the list.
+"""Day 1 - Part 1:
 
-For example:
+The captcha requires you to review a sequence of digits and find the
+sum of all digits that match the next digit in the list. The list is
+circular, so the digit after the last digit is the first digit in the
+list. For example:
 
 >>> solve("1122")
 3
@@ -14,11 +15,28 @@ For example:
 0
 >>> solve("91212129")
 9
+
+Part 2:
+
+Instead of considering the next digit, consider the digit halfway around
+the circular list. The list has an even number of elements. For example:
+
+>>> solve("1212", part2=True)
+6
+>>> solve("1221", part2=True)
+0
+>>> solve("123425", part2=True)
+4
+>>> solve("123123", part2=True)
+12
+>>> solve("12131415", part2=True)
+4
 """
 
 
-def solve(captcha):
-    next_digit_comparisons = zip(captcha, captcha[1:] + captcha[0])
+def solve(captcha, part2=False):
+    i = int(len(captcha) / 2) if part2 else 1
+    next_digit_comparisons = zip(captcha, captcha[i:] + captcha[:i])
     return sum([int(d) for d, d2 in next_digit_comparisons if d == d2])
 
 
@@ -27,3 +45,4 @@ if __name__ == "__main__":
         puzzle_input = f.read()
 
     print(solve(puzzle_input))
+    print(solve(puzzle_input, part2=True))
